7a64dff2f65e6c0a1927e1cb31a4bed01da894cc
Bhuwan Bhatt <bhattbhuwan13@outlook.com>
2021-09-26 06:18:38 +0545

    check for without venve

---
+++ new_hello.py
@@ -0,0 +1,26 @@
+import math
+import os
+
+
+def hello():
+
+    print("hello")
+
+
+def new(strin="hi"):
+
+    print(strin)
+
+
+def check(i=1, j=2):
+    print(i, j)
+
+
+def new_one(st="right"):
+    print(st + "hi")
+
+
+def error_func(t="dfjkj"):
+    x = 2
+    b = 1
+    return x + b
